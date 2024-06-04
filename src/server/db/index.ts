4ef776fdd26d1ff4e758a8f42793096f1887a216
Samnan Rahee <samnan.rahee.96@gmail.com>
2024-06-04 17:07:50 +0600

    Init project

---
+++ src/server/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import { env } from "@/env";
+import * as schema from "./schema";
+
+const conn = neon(env.DATABASE_URL);
+export const db = drizzle(conn, { schema });
